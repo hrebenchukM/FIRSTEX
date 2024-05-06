3fc003181099abca1bdf21b08249989daea91ab3
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-05-06 17:33:06 +0300

    ..

---
+++ first.js
@@ -0,0 +1,106 @@
+var Student = /** @class */ (function () {
+    function Student(_name, _birthDate, _contactPhone, _city, _country, _company, _groupNumber) {
+        this._name = _name;
+        this._birthDate = _birthDate;
+        this._contactPhone = _contactPhone;
+        this._city = _city;
+        this._country = _country;
+        this._company = _company;
+        this._groupNumber = _groupNumber;
+    }
+    Object.defineProperty(Student.prototype, "name", {
+        // accessors - механизм, который позволяет перехватить обращение к члену объекта на запись или чтение.
+        // getter 
+        get: function () {
+            return this._name;
+        },
+        // setter
+        set: function (value) {
+            this._name = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Student.prototype, "birthDate", {
+        get: function () {
+            return this._company;
+        },
+        set: function (value) {
+            this._company = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Student.prototype, "contactPhone", {
+        get: function () {
+            return this._company;
+        },
+        set: function (value) {
+            this._company = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Student.prototype, "city", {
+        get: function () {
+            return this._company;
+        },
+        set: function (value) {
+            this._company = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Student.prototype, "country", {
+        get: function () {
+            return this._company;
+        },
+        set: function (value) {
+            this._company = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Student.prototype, "company", {
+        get: function () {
+            return this._company;
+        },
+        set: function (value) {
+            this._company = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Student.prototype, "groupNumber", {
+        get: function () {
+            return this._groupNumber;
+        },
+        set: function (value) {
+            this._groupNumber = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Student.prototype.printToConsole = function () {
+        console.log("Student ".concat(this._name, ", birthDate - ").concat(this._birthDate, ", contactPhone - ").concat(this._contactPhone, ",\n        city - ").concat(this._city, ", country - ").concat(this._country, ", company - ").concat(this._company, ", groupNumber - ").concat(this._groupNumber));
+    };
+    Student.prototype.printToTable = function () {
+        var table = document.getElementById('studentTable');
+        if (table !== null) { // TypeScript не знает, что table= HTML-элемент,ибо getElementById может вернуть null.?
+            var row = document.createElement('tr');
+            row.innerHTML = "\n                <td>".concat(this._name, "</td>\n                <td>").concat(this._birthDate, "</td> \n                <td>").concat(this._contactPhone, "</td>\n                <td>").concat(this._city, "</td>\n                <td>").concat(this._country, "</td>\n                <td>").concat(this._company, "</td>\n                <td>").concat(this._groupNumber, "</td>\n            ");
+            table.appendChild(row);
+        }
+        else {
+            console.error("Table element not found!");
+        }
+    };
+    return Student;
+}());
+var students = [
+    new Student("Mary Hrebenchuk", "2004-12-10", "+380972860462", "Odesa", "Ukraine", "Step", "P222"),
+    new Student("Harry Potter", "1980-07-31", "+380995410272", "Odesa", "Ukraine", "Step", "P223")
+];
+students.forEach(function (student) {
+    student.printToTable();
+});
---
+++ first.ts
@@ -0,0 +1,113 @@
+
+
+class Student {
+    
+    private _name: string;// модификатор доступа private - указывает что данное поле закрытое и не доступно за пределами класса
+    private _birthDate: string;
+    private _contactPhone: string;
+    private _city: string;
+    private _country: string;
+    private _company: string;
+    private _groupNumber: string;
+
+
+    constructor(_name: string, _birthDate: string,_contactPhone: string,_city: string,_country: string,_company: string,_groupNumber: string) {
+        this._name = _name;
+        this._birthDate = _birthDate;
+        this._contactPhone = _contactPhone;
+        this._city = _city;
+        this._country = _country;
+        this._company =_company;
+        this._groupNumber = _groupNumber;
+    }
+
+    // accessors - механизм, который позволяет перехватить обращение к члену объекта на запись или чтение.
+
+    // getter 
+    get name(): string {
+        return this._name;
+    }
+    get birthDate(): string {
+        return this._company;
+    }
+    get contactPhone(): string {
+        return this._company;
+    }
+    get city(): string {
+        return this._company;
+    }
+    get country(): string {
+        return this._company;
+    }
+    get company(): string {
+        return this._company;
+    }
+    get groupNumber(): string {
+        return this._groupNumber;
+    }
+
+
+
+
+
+
+    // setter
+    set name(value: string) {
+        this._name = value;
+    }
+    set birthDate(value: string) {
+        this._company = value;
+    }
+    set contactPhone(value: string) {
+        this._company = value;
+    }
+    set city(value: string) {
+        this._company = value;
+    }
+    set country(value: string) {
+        this._company = value;
+    }
+    set company(value: string) {
+        this._company = value;
+    }
+    set groupNumber(value: string) {
+        this._groupNumber = value;
+    }
+
+
+    printToConsole() {
+        console.log(`Student ${this._name}, birthDate - ${this._birthDate}, contactPhone - ${this._contactPhone},
+        city - ${this._city}, country - ${this._country}, company - ${this._company}, groupNumber - ${this._groupNumber}`);
+    }
+
+    printToTable(): void {
+        let table = document.getElementById('studentTable');
+        if (table !== null) {// TypeScript не знает, что table= HTML-элемент,ибо getElementById может вернуть null.?
+            let row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${this._name}</td>
+                <td>${this._birthDate}</td> 
+                <td>${this._contactPhone}</td>
+                <td>${this._city}</td>
+                <td>${this._country}</td>
+                <td>${this._company}</td>
+                <td>${this._groupNumber}</td>
+            `;
+            table.appendChild(row);
+        } else {
+            console.error("Table element not found!");
+        }
+    }
+    
+    
+}
+
+let students: Student[] = [
+    new Student("Mary Hrebenchuk", "2004-12-10", "+380972860462", "Odesa", "Ukraine", "Step", "P222"),
+    new Student("Harry Potter", "1980-07-31", "+380995410272", "Odesa", "Ukraine", "Step", "P223")
+];
+
+
+students.forEach(student => {
+    student.printToTable();
+});
